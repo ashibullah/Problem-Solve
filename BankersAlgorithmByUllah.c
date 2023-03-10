80792e723001f2cd659e667a1de09730d7fd4a5c
Ashib Ullah <86849395+ashibullah@users.noreply.github.com>
2023-03-11 02:21:38 +0600

    Add files via upload

---
+++ BankersAlgorithmByUllah.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+int main()
+{
+    int m, n;
+    printf("Processes: ");
+    scanf("%d", &m);
+    printf("Resources: ");
+    scanf("%d",&n);
+    int ins[n], pro[m][n], Max[m][n], Av[m][n], need[m][n], count[n];
+    int c = 0, d = 0, ne[m], ave[m], maaax[m], al[m], finish[m];
+    // taking input
+    printf("Instances of\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%c ",65+i);
+    }
+    puts("");
+    
+    for (int i = 0; i < n; i++) // ins
+    {
+        scanf("%d", &ins[i]);
+    }
+    puts("Allocations of");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%c ",65+i);
+    }
+    puts("");
+    for (int i = 0; i < m; i++) // allocations
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &pro[i][j]);
+        }
+    }
+
+    puts("Max of"); // Input max
+    for (int i = 0; i < n; i++)
+    {
+        printf("%c ",65+i);
+    }
+    puts("");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &Max[i][j]);
+        }
+    }
+    printf("Available\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%c ",65+i);
+    }
+    puts("");
+    for (int j = 0; j < n; j++) // available
+    {
+        scanf("%d", &Av[0][j]);
+        count[j] = Av[0][j];
+    }
+    // Find Out Need
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            need[i][j] = Max[i][j] - pro[i][j];
+        }
+    }
+    // converting available storing it in c & instance in c
+    for (int i = 0; i < sizeof(count) / sizeof(count[0]); i++) // av 7 5 3 =753
+    {
+        c = c * 10 + count[i];
+    }
+    /* 
+    for (int i = 0; i < sizeof(count) / sizeof(count[0]); i++)
+    { 
+        d = d * 10 + ins[i];
+    }
+    */
+    // convert need and alocation
+    for (int i = 0; i < m; i++)
+    {
+        int num=0;
+        for (int j = 0; j < n; j++)
+        {
+            num = num * 10 + need[i][j];
+        }
+        ne[i]=num;
+    }
+    for (int i = 0; i < m; i++)
+    {   int num=0;
+        for (int j = 0; j < n; j++)
+        {
+            num = num * 10 + pro[i][j];
+        }
+        al[i]=num;
+    }
+    
+
+    int step=0,gant[m];
+    while (step<=m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            if (ne[i] <= c)
+            {
+                c = c + al[i];
+                finish[i] = 1;
+                gant[step]= i+1;
+                step++;
+                
+            }
+        }
+        
+    }
+    puts("Process Schedule Gant Chart");
+    for (int i = 0; i < m; i++)
+        {
+            printf("[P%d] ", gant[i]);
+        }
+    puts("\nBoolean Value of Finish");
+    for (int i = 0; i < m; i++)
+        {
+            printf("[ %d ]", finish[i]);
+        }
+        getchar();
+    //     puts("");
+    // for (int i = 0; i < 5; i++)
+    //     {
+    //         printf("%d ", ne[i]);
+    //     }
+    /* int finished = 0;
+    while (finished < m)
+    {
+        // try to find a process that can be finished
+        int found = 0;
+        for (int i = 0; i < m; i++)
+        {
+            if (finish[i] == 0)
+            {
+                int can_finish = 1;
+                for (int j = 0; j < n; j++)
+                {
+                    if (need[i][j] > count[j])
+                    {
+                        can_finish = 0;
+                        break;
+                    }
+                }
+                if (can_finish)
+                {
+                    // mark the process as finished and update available resources
+                    finish[i] = 1;
+                    for (int j = 0; j < n; j++)
+                    {
+                        count[j] += pro[i][j];
+                    }
+                    found = 1;
+                    finished++;
+                }
+            }
+        }
+        // if no process can be finished, then we are in an unsafe state
+        if (!found)
+        {
+            printf("Unsafe state detected\n");
+            break;
+        }
+    }
+    */
+
+    /*
+    int d = equal(ins,count,n);
+    while (d==1)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (need[i][j]<=count[j])
+                {
+
+
+                }
+
+
+            }
+        }
+    }
+    */
+
+    return 0;
+}
